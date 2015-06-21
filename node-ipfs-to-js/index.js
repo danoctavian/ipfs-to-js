8d5f8d99ab0a5753dfb3a55f75ceca158ebba4b8
dan <danoctavian91@gmail.com>
2015-06-20 18:07:25 -0700

    added more test code. fails on unimplemented system call

--- node-ipfs-to-js/index.js
+++ node-ipfs-to-js/index.js
@@ -1,7 +1,12 @@
 require('node-go-require')
 
+console.log("loading go code...")
+
 var mainGo = require("../main.go")
 
 
 console.log("wtf man")
 console.log(mainGo.ops.add(2, 5))
+mainGo.ops.print("holy shit")
+
+mainGo.ops.readFile("/home/dan/hello")
